e4d411bb72b5e852af00963122ca2e10cc33fa9b
Miquéias Bito <miqueiasbito@gmail.com>
2026-06-13 12:52:10 -0300

    Cria primeiras classes do projeto

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Pessoa pessoa1 = new Pessoa();
+
+        pessoa1.nome = "Miqueias";
+        pessoa1.idade = 23;
+        pessoa1.peso = 86.0;
+        pessoa1.altura = 1.80;
+
+        System.out.println("Nome: " + pessoa1.nome);
+        System.out.println("Idade: " + pessoa1.idade);
+        System.out.println("Peso: " + pessoa1.peso);
+        System.out.println("Altura: " + pessoa1.altura);
+    }
+}
