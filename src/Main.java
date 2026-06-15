0c3bdc18f4fa69017af0bf6f75903ca08313c7d6
Miquéias Bito <miqueiasbito@gmail.com>
2026-06-14 21:28:47 -0300

    Melhora entrada de altura do usuário

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
     
     public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("=== Calculadora de IMC ===");
+
+        System.out.println("Digite o nome: ");
+        String nome = entrada.nextLine();
+
+        System.out.println("Digite a idade: ");
+        int idade = entrada.nextInt();
+
+        System.out.println("Digite o peso: ");
+        double peso = entrada.nextDouble();
+
+        // Lê a altura digitada pelo usuário
+        System.out.println("Digite sua altura em metros. Exemplo: 1,76: ");
+        double altura = entrada.nextDouble();
+
+        // Se o usuário digitar 176, o programa entende que foi em centímetros
+        // Caso o usuário digite 176 em vez de 1,76
+        // O programa converte para de centímetros para metros
+        if (altura > 3) {
+            altura = altura / 100;
+        }
         
-        Pessoa pessoa1 = new Pessoa();
+        Pessoa pessoa1 = new Pessoa(nome, idade, peso, altura);
 
-        pessoa1.nome = "Miqueias";
-        pessoa1.idade = 23;
-        pessoa1.peso = 86.0;
-        pessoa1.altura = 1.80;
-
+        System.out.println("\n=== Resultado ===");
         System.out.println("Nome: " + pessoa1.nome);
         System.out.println("Idade: " + pessoa1.idade);
         System.out.println("Peso: " + pessoa1.peso);
@@ -18,5 +40,7 @@ public class Main {
 
         System.out.println("IMC: " + imc);
         System.out.println("Classificação: " + pessoa1.classificarIMC());
+
+        entrada.close();
     }
 }
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,13 +1,25 @@
 public class Pessoa {
+
+    // Atributos; responsavel por características que toda pessoa terá
     String nome;
     int idade;
     double peso;
     double altura;
 
+    // Construtor: usado para criar uma pessoa já com dados preenchidos
+    public Pessoa (String nome, int idade, double peso, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    // Método responsável por calcular o IMC da pessoa
     double calcularIMC() {
         return peso / (altura * altura);
     }
 
+    // Método responsável por classificar o IMC calculado
     String classificarIMC() {
         double imc = calcularIMC();
         if (imc < 18.5) {
